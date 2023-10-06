a7e93df66fc46f5e1ff5fdb7a136f5ef88596f71
xodndlee <dnd5436@gmail.com>
2023-10-06 12:04:22 +0900

    change

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-    process.env.NODE_ENV === 'develpment' ? 'http://localhost:8080' : 'http://10.128.0.5:8080/';
+    process.env.NODE_ENV === 'develpment' ? 'http://localhost:8080' : 'http://34.30.0.136:8080/';
 
 export const Api = async (url, method, data) => {
     const body = await axios({
